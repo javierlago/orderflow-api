97acd94197562d2443aba0e8b681852369bb67ea
Javier Lago Amoedo <javierlagoamoedo@gmail.com>
2025-12-24 19:00:10 +0100

    feat: register OrderFlowDbContext in API DI

--- src/OrderFlow.Api/Program.cs
+++ src/OrderFlow.Api/Program.cs
@@ -1,8 +1,22 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using OrderFlow.Api.Validation;
+using Microsoft.EntityFrameworkCore;
+using OrderFlow.Infrastructure.Persistence;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateCustomerRequestValidator>();
+builder.Services.AddDbContext<OrderFlowDbContext>(options =>
+{
+    var cs = builder.Configuration.GetConnectionString("OrderFlowDb");
+    options.UseSqlServer(cs);
+});
 
 var app = builder.Build();
 
